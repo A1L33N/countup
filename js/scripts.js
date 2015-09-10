79cf02561a40956786672efa2e329cab01971eb2
Aileen Cacayorin & Evan Clough <student@epicodus.com>
2015-09-10 09:58:57 -0700

    Build index page.

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,19 @@ var countUp = function(countTo, countBy) {
   };
   return count;
 };
+
+$(document).ready(function(){
+  $('form#count-up').submit(function(event){
+    var countTo = parseInt($('input#count_to').val());
+    var countBy = parseInt($('input#count_by').val());
+    if(countBy > countTo) {
+      alert("You must count to a number higher than your count interval. Please check your inputs.")
+    } else {
+      var count = countUp(countTo, countBy);
+      var countString = count.join(', ');
+
+      $('#count-result').text(countString);
+      event.preventDefault();
+    };
+  });
+});
